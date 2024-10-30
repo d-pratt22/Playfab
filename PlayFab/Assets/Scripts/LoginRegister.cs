4e3bce2eee0230b8f0c8ef37628fa5862948bcbc
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2024-10-29 18:58:38 -0700

    DEV1

---
+++ PlayFab/Assets/Scripts/LoginRegister.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.Events;
+
+public class LoginRegister : MonoBehaviour
+{
+    [HideInInspector]
+    public string playFabId;
+
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+    public TextMeshProUGUI displayText;
+
+    public UnityEvent onLoggedIn;
+
+    public static LoginRegister instance;
+    void Awake() { instance = this; }
+
+    public void OnRegister()
+    {
+        RegisterPlayFabUserRequest registerRequest = new RegisterPlayFabUserRequest
+        {
+            Username = usernameInput.text,
+            DisplayName = usernameInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest,
+ result =>
+ {
+     Debug.Log(result.PlayFabId);
+ },
+ error =>
+ {
+     Debug.Log(error.ErrorMessage);
+ }
+);
+    }
+
+    public void OnLoginButton()
+    {
+        LoginWithPlayFabRequest loginRequest = new LoginWithPlayFabRequest
+        {
+            Username = usernameInput.text,
+            Password = passwordInput.text
+        };
+
+        PlayFabClientAPI.LoginWithPlayFab(loginRequest,
+            result => {
+                SetDisplayText("Logged in as: " + result.PlayFabId, Color.green);
+
+                if (onLoggedIn != null)
+                    onLoggedIn.Invoke();
+                playFabId = result.PlayFabId;
+
+            },
+            error => SetDisplayText(error.ErrorMessage, Color.red));
+
+    }
+
+    void SetDisplayText(string text, Color color)
+    {
+        displayText.text = text;
+        displayText.color = color;
+    }
+
+}
